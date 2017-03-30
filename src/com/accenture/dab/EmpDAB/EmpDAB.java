0e4ed5183dc08d7c1c68e274be8c1498a500ddf5
shubhamgoel7703 <shubhamgoel7703@gmail.com>
2017-03-30 12:40:25 +0530

    Update EmpDAB.java
    GitFunction added

--- src/com/accenture/dab/EmpDAB/EmpDAB.java
+++ src/com/accenture/dab/EmpDAB/EmpDAB.java
@@ -29,6 +29,8 @@ public class EmpDAB extends BaseDAB /*implements IEmpDAB*/ {
 	    		  if(instance==null)
 	    		  instance = new EmpDAB();
 	    		  }
+		      
+		      // gitFunction();
 	      }  
 	      return instance;
 	   }
